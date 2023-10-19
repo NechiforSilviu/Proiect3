393c7e4a43ec0bc99f05370a1e06d278cbbf4ca0
NechiforSilviuM <108395482+NechiforSilviu@users.noreply.github.com>
2023-10-19 15:08:40 +0300

    Update mainDAFX.m

--- mainDAFX.m
+++ mainDAFX.m
@@ -6,7 +6,7 @@ x = x(1:1024);
 X = fft(x);
 N = length(X);
 
-filter_type = [1 1 1 1 1];
+% filter_type = [1 1 1 1 1];
 for i = 1:5
 
     [y] = CreateFilterDAFX(x,fs,i);
@@ -17,11 +17,12 @@ for i = 1:5
     figure(1)
     subplot(211)
     plotOnAudibleRange(x, fs);
-    
     subplot(212);
     plotOnAudibleRange(y, fs);
+
     sound(y1,fs);
     pause(3);
+
 end
 
 
